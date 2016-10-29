a3ae0b1ecd2e6a1992a90d055214d3b537625eb6
Grad1m <dimagraj@gmail.com>
2016-10-29 18:05:03 +0300

    New interface ConnectionFactory and his impl. for Connection object. New class ConnectionFactoryImpl with that interface for DB connection

---
+++ usermanagement/src/main/java/kn/dmytro_grazhevskiy/usermanagement/db/ConnectionFactoryImpl.java
@@ -0,0 +1,29 @@
+package kn.dmytro_grazhevskiy.usermanagement.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactoryImpl implements ConnectionFactory {
+
+	@Override
+	public Connection createConnection() throws DatabaseException {
+		String url = "jdbc:hsqldb:file:db/usermanagement";
+		String user="sa";
+		String password="";
+		String driver = "org.hsqldb.jdbcDriver";
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+			//e.printStackTrace();
+		}
+		
+		try {
+			return DriverManager.getConnection(url, user,password);
+		} catch (SQLException e) {
+			throw new DatabaseException(e);
+		}
+	}
+
+}
