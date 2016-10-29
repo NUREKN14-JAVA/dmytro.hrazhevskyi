99a8687a6a54f3052b759418e66d37c4c1bebd23
Grad1m <dimagraj@gmail.com>
2016-10-29 20:48:45 +0300

    Adding default constrct. into HsqldbUserDao class

--- usermanagement/src/main/java/kn/dmytro_grazhevskiy/usermanagement/db/UserDao.java
+++ usermanagement/src/main/java/kn/dmytro_grazhevskiy/usermanagement/db/UserDao.java
@@ -10,4 +10,5 @@ void update(User user) throws DatabaseException;
 void delete(User user) throws DatabaseException;
 User find(Long id) throws DatabaseException;
 Collection findAll() throws DatabaseException;
+void setConnectionFactory(ConnectionFactory connectionFactory);
 }
