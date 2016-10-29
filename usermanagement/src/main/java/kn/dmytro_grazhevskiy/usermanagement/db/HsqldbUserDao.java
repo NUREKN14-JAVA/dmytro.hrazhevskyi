9754cf43e554c91b46164ab668264d1bfb011400
Grad1m <dimagraj@gmail.com>
2016-10-29 19:07:34 +0300

    Impl. of create() method

--- usermanagement/src/main/java/kn/dmytro_grazhevskiy/usermanagement/db/HsqldbUserDao.java
+++ usermanagement/src/main/java/kn/dmytro_grazhevskiy/usermanagement/db/HsqldbUserDao.java
@@ -1,11 +1,16 @@
 package kn.dmytro_grazhevskiy.usermanagement.db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Collection;
+import java.sql.Date;
 
 import kn.dmytro_grazhevskiy.usermanagement.User;
 
 public class HsqldbUserDao implements UserDao {
 
+private static final String INSERT_QUERY = "INSERT INTO users(firstname, lastname, dateofbirth) VALUES (?, ?, ?)";
 private ConnectionFactory connectionFactory;
 
 
@@ -16,8 +21,23 @@ public HsqldbUserDao(ConnectionFactory connectionFactory) {
 
 
 	public User create(User user) throws DatabaseException {
-		// TODO Auto-generated method stub
-		return null;
+		try {Connection connection = connectionFactory.createConnection();
+			PreparedStatement statement = connection.prepareStatement(INSERT_QUERY);
+			statement.setString(1, user.getFirstName());
+			statement.setString(2,user.getLastName() );
+			statement.setDate(3,new Date(user.getDateOfBirth().getTime()) );
+			int n = statement.executeUpdate();
+			if(n!=1){
+				throw new DatabaseException("Number of the inserted rows: "+ n);
+				
+			}
+			return null;
+		} catch(DatabaseException e){
+			throw e;
+		}catch (SQLException e) {
+			throw new DatabaseException(e);
+		}
+	
 	}
 
 
