59198849a5314b66095694939f72739775f99fad
Grad1m <dimagraj@gmail.com>
2016-10-29 19:47:19 +0300

    Changing HsqldbUserDao class for testing findall(). Extends from DatabaseTestCase and new methods getConenction(), getDataSet()

--- usermanagement/src/test/java/kn/dmytro_grazhevskiy/usermanagement/db/HsqldbUserDaoTest.java
+++ usermanagement/src/test/java/kn/dmytro_grazhevskiy/usermanagement/db/HsqldbUserDaoTest.java
@@ -4,13 +4,18 @@ import static org.junit.Assert.*;
 
 import java.util.Date;
 
+import org.dbunit.DatabaseTestCase;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.XmlDataSet;
 import org.junit.Before;
 import org.junit.Test;
 
 import junit.framework.TestCase;
 import kn.dmytro_grazhevskiy.usermanagement.User;
 
-public class HsqldbUserDaoTest extends TestCase {
+public class HsqldbUserDaoTest extends DatabaseTestCase {
 
 	 private HsqldbUserDao dao;
 	 private ConnectionFactory connectionFactory;
@@ -18,7 +23,7 @@ public class HsqldbUserDaoTest extends TestCase {
 		@Before
 		protected void setUp() throws Exception {
 			super.setUp();
-			connectionFactory=new ConnectionFactoryImpl();
+			
 			dao = new HsqldbUserDao(connectionFactory);
 			
 		}
@@ -41,6 +46,19 @@ public class HsqldbUserDaoTest extends TestCase {
 		}
 		 
 		 
+		}
+
+		@Override
+		protected IDatabaseConnection getConnection() throws Exception {
+			connectionFactory=new ConnectionFactoryImpl();
+			return new DatabaseConnection(connectionFactory.createConnection()) ;
+			
+		}
+
+		@Override
+		protected IDataSet getDataSet() throws Exception {
+		IDataSet dataSet = new XmlDataSet(getClass().getClassLoader().getResourceAsStream("userDataSet.xml"));
+			return dataSet;
 		}
 
 
