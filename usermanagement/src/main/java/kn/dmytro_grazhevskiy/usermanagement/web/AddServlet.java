2af3fa007093f65bc77761334998e7cfbe357cc1
DGrazhevsky <dimagraj@gmail.com>
2016-12-10 22:25:15 +0300

    AddServletTEst class is ready

--- usermanagement/src/main/java/kn/dmytro_grazhevskiy/usermanagement/web/AddServlet.java
+++ usermanagement/src/main/java/kn/dmytro_grazhevskiy/usermanagement/web/AddServlet.java
@@ -1,7 +1,26 @@
 package kn.dmytro_grazhevskiy.usermanagement.web;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class AddServlet extends HttpServlet {
+import kn.dmytro_grazhevskiy.usermanagement.User;
+import kn.dmytro_grazhevskiy.usermanagement.db.DaoFactory;
+import kn.dmytro_grazhevskiy.usermanagement.db.DatabaseException;
 
+public class AddServlet extends EditServlet {
+
+@Override
+    protected void processUser(User user) throws DatabaseException {
+        DaoFactory.getInstance().getUserDao().create(user);
+    }
+
+@Override
+    protected void showPage(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/add.jsp").forward(req, resp);
+    }
 }
