9911a64215a05bad260e7d523b750a1fbffc5a27
DGrazhevsky <dimagraj@gmail.com>
2016-12-11 19:23:58 +0300

    Minor changes

--- usermanagement/src/test/java/kn/dmytro_grazhevskiy/usermanagement/gui/MockUserDao.java
+++ usermanagement/src/test/java/kn/dmytro_grazhevskiy/usermanagement/gui/MockUserDao.java
@@ -56,8 +56,7 @@ public class MockUserDao implements UserDao {
 
 	@Override
 	public Collection find(String firstName, String lastName) throws DatabaseException {
-		// TODO Auto-generated method stub
-		return null;
+throw new UnsupportedOperationException();
 	}
 
 
